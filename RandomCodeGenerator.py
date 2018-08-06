59c667d1783371612287178a9e2aef742a05b16a
Mar_Mich <42131507+mariosfish@users.noreply.github.com>
2018-08-06 23:40:55 +0300

    Added input prompt and digits variable.

--- RandomCodeGenerator.py
+++ RandomCodeGenerator.py
@@ -3,13 +3,15 @@ import string
 
 print('Random Code Generator')
 
+digits= int(input('Enter the desire number of digits of your code: '))
 code = ''
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-for i in range(0, 26):
-	if i % 4 == 0:
+for i in range(0, digits):
+	if i % random.choice(numbers) == 0:
 		code = code + random.choice(string.ascii_letters)
 	else:
 		code = code + str(random.choice(numbers))
 
-print code
+print (code)
+print (len(code))
