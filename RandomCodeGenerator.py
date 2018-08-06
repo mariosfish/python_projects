c0ed0f7bce6a01d72387b6c45840904e9dc4224e
Mar_Mich <mariosfish1@gmail.com>
2018-08-06 14:30:29 +0300

    Nothing special. again. test
    Signed-off-by: Mar_Mich <mariosfish1@gmail.com>

--- RandomCodeGenerator.py
+++ RandomCodeGenerator.py
@@ -7,7 +7,7 @@ code = ''
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 for i in range(0, 26):
-	if i % 3 == 0:
+	if i % 4 == 0:
 		code = code + random.choice(string.ascii_letters)
 	else:
 		code = code + str(random.choice(numbers))
