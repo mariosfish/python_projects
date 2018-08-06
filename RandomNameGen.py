6d0c3252e38ff9680fbad17f59f14ce94a875dce
Mar_Mich <mariosfish1@gmail.com>
2018-08-06 12:23:08 +0300

    Nothing special. again. test
    Signed-off-by: Mar_Mich <mariosfish1@gmail.com>

--- RandomNameGen.py
+++ RandomNameGen.py
@@ -5,18 +5,8 @@ print('Random name generator')
 print
 print
 
-name=""
-for i in range(0,5):
-	letter=random.choice(string.ascii_uppercase)
-	name= name+letter
+name = ""
+for i in range(0, 5):
+	letter = random.choice(string.ascii_uppercase)
+	name = name + letter
 print name
-
-print("test")
-
-print('marios')
-print('marios')
-print('marios')
-print('marios')
-print('marios')
-print('marios')
-print('marios')
