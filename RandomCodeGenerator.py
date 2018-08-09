d292e1f876c507971c51d14a9f59532fcf0a17fc
mariosfish <mariosfish1@gmail.com>
2018-08-09 21:15:06 +0300

    test again

--- RandomCodeGenerator.py
+++ RandomCodeGenerator.py
@@ -4,15 +4,18 @@ import math
 
 print('Random Code Generator')
 
-digits= int(input('Enter the desire number of digits of your code: '))
+digits = int(input('Enter the desire number of digits of your code: '))
 code = ''
-numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 for i in range(0, digits):
-	if i % random.choice(numbers) == 0:
-		code = code + random.choice(string.ascii_letters)
-	else:
-		code = code + str(random.choice(numbers))
+    if i % random.choice(numbers) == 0:
+        code = code + random.choice(string.ascii_letters)
+    else:
 
-print (code)
-print (len(code))
+        code = code + str(random.choice(numbers))
+
+print(code)
+print(len(code))
