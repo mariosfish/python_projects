a4a024ceeac723004d87f790da0a0853d40c2ccf
mariosfish <mariosfish1@gmail.com>
2018-08-09 23:03:22 +0300

    Making the while works.

--- RandomCodeGenerator.py
+++ RandomCodeGenerator.py
@@ -1,21 +1,24 @@
+import math
+import numbers
 import random
 import string
-import math
+
+code = ''
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+digits = ''
 
 print('Random Code Generator')
+digits = input('Enter the desire number of digits of your code: ')
 
-digits = int(input('Enter the desire number of digits of your code: '))
-code = ''
-
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-for i in range(0, digits):
-    if i % random.choice(numbers) == 0:
-        code = code + random.choice(string.ascii_letters)
+while digits.isalpha():
+    digits = input('Enter the desire number of digits of your code: ')
+else:
+    for i in range(0, int(digits)):
+        if i % random.choice(numbers) == 0:
+            code = code+random.choice(string.ascii_letters)
     else:
+        code = code+str(random.choice(numbers))
 
-        code = code + str(random.choice(numbers))
 
 print(code)
 print(len(code))
