4ccb0392327fab14355d1884197816cc22e68bf6
Mar_Mich <42131507+mariosfish@users.noreply.github.com>
2020-03-03 22:10:15 +0200

    made some fields

--- adeies/main_window.py
+++ adeies/main_window.py
@@ -3,7 +3,7 @@ from tkinter import *
 root = Tk()
 root.title('Άδεια Στρατιωτικού Προσωπικού')
 root.iconbitmap('')
-root.geometry('400x400')
+root.geometry('800x400')
 
 # Labels
 bathmos = Label(root, text="Βαθμός:")
@@ -20,25 +20,32 @@ eidos_adeias = Label(root, text="Είδος άδειας:")
 bathmos_entry = Entry(root, width=20)
 am_entry = Entry(root, width=10)
 onoma_entry = Entry(root, width=20)
-eponymo_entry = Entry(root, width=25)
+eponymo_entry = Entry(root, width=210)
 patronymo_entry = Entry(root, width=20)
 kathikonta_entry = Entry(root, width=20)
 bathmos_entry = Entry(root, width=20)
 edra_entry = Entry(root, width=10)
-thema_entry = Entry(root, width=20)
+thema_entry = Entry(root, width=40)
 eidos_entry = Entry(root, width=10)
 
+# Buttons
+create = Button(root, text='Δημιουργία άδειας')
 
 # Positioning
-bathmos.grid(row=0, column=0,pady=5)
-bathmos_entry.grid(row=0, column=1,pady=5)
-am.grid(row=0, column=2, padx=5, pady=5)
+bathmos.grid(row=0, column=0, pady=5, sticky=W, padx=10)
+bathmos_entry.grid(row=0, column=1, pady=5)
+am.grid(row=0, column=2, padx=10, pady=5)
 am_entry.grid(row=0, column=3, pady=5)
 onoma.grid(row=1, column=0, pady=5)
-onoma_entry.grid(row=1, column=0, pady=5)
-bathmos_entry.grid(row=0, column=1, pady=5)
-am.grid(row=0, column=2, padx=5, pady=5)
-am_entry.grid(row=0, column=3, pady=5)
-
+onoma_entry.grid(row=1, column=1, pady=5)
+eponymo.grid(row=1, column=2, pady=5)
+eponymo_entry.grid(row=1, column=3, pady=5)
+patronymo.grid(row=2, column=0, pady=5)
+patronymo_entry.grid(row=2, column=1, pady=5)
+kathikonta.grid(row=2, column=2, pady=5)
+kathikonta_entry.grid(row=2, column=3, pady=5)
+thema.grid(row=3, column=0, pady=5)
+thema_entry.grid(row=3, column=1, pady=5)
+create.grid(row=6, column=0, padx=10, pady=5)  # Button
 
 root.mainloop()
