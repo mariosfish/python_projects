23e2dde0f1683ed3cac3915bf30496e887101e15
Mar Mich <mariosfish1@gmail.com>
2020-08-18 23:38:11 +0300

    made changes with docstrings

--- square_n_multipy_modulo.py
+++ square_n_multipy_modulo.py
@@ -1,28 +1,40 @@
 class SqMl():
 
-    """This is an implementation of the square and multiply algorithm in some 
-    modulo n in Python. The general form is ' (b^exp) % n '.\n
-    base--> Takes an integer as a base.\n
-    exp--> Takes an integer as an exponent of the base.\n
-    mod--> Takes an integer as the modulo parameter n. Default value is 1.\n
-    This algorithm is very usefull in cryptography and has O(log n) 
-    computational time for big exponents."""
-
     # author: "MrMariosfish"
 
     # constructor
     def __init__(self, base, exp, mod):
+        '''
+        __init__ Constructor of a class object.
+
+        Arguments:
+            base {Integer} -- Takes an integer as a base.
+            exp {Integer} -- Takes an integer as an exponent of the base.
+            mod {Integer} -- Takes an integer as the modulo parameter n. Default value is 1.
+        '''
         self.base = int(base)
         self.exp = int(exp)
         self.mod = int(mod)
 
     # convert the exponent from decimal->binary
     def decToBin(self):
+        '''
+        decToBin Converts the exponent from decimal to binary.
+
+        Returns:
+            List -- List with binary digits of the exponent.
+        '''
         decUpper = bin(self.exp)[2:]
         return list(decUpper)
 
     # calculates the remainder of the (base^exp) % n
     def sqAndMul(self):
+        '''
+        sqAndMul Calculates the remainder of the (base^exp) % n.
+
+        Returns:
+            Integer -- Returns the remainder of the (base^exp) % n.
+        '''
         product = self.base
         for i in self.decToBin()[1:]:
             if i == "0":
